d05448d3490377edeb3f94e74ce26238afbb7e16
GajanandYadav1 <gajanandsabalpura06@gmail.com>
2025-06-30 10:02:39 +0530

    wer

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -6,7 +6,7 @@ const RootLayout = () => {
   return (
   <Stack>
     <Stack.Screen name='(drawer)' options={{headerShown: false}}/>
-    <Stack.Screen name='(tab)'  options={{headerShown: false}}/>
+    <Stack.Screen name='(tab)' options={{headerShown: false}}/>
     <Stack.Screen name='index'/> 
     <Stack.Screen name='about'/> 
   </Stack>
