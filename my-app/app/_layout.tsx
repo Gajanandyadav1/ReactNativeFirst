83d58b283b2a48c6804a81ec8c54af88a18a4024
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2025-07-24 11:40:12 +0530

    cnamehange

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
   <Stack screenOptions={{headerShown:false}}>
     <Stack.Screen  name='index'/> 
     
-    <Stack.Screen  name='login'/>
+    <Stack.Screen  name='login' />
     <Stack.Screen  name='signup'/>
   </Stack>
   )
